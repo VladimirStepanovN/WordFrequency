365e9d58ac0717ae701e47b54a4944a117d30a64
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-12-28 00:19:55 +0500

    Задание 13.6.2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,46 @@
-﻿namespace WordFrequency
+﻿namespace WordFrequency;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        if (args.Length > 0 && !string.IsNullOrWhiteSpace(args[0]))
         {
-            Console.WriteLine("Hello, World!");
+            string[] text;
+            try
+            {
+                using (StreamReader reader = File.OpenText(args[0]))
+                {
+                    text = (new string(reader.ReadToEnd().Where(c => !char.IsPunctuation(c)).ToArray())).Split();
+                }
+                var result = text
+                    .Where(word => !string.IsNullOrWhiteSpace(word))
+                    .GroupBy(word => word)
+                    .OrderByDescending(group => group.Count())
+                    .Select(group => group.Key)
+                    .Take(10);
+                foreach (var item in result)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.Read();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"Файл {args[0]} не найден.");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Путь к файлу или папке содержит недопустимые символы.");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Недопустимое действие или недостаточно прав на чтение файлов.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
